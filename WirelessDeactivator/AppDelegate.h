c3909c58df9e63c7fd2fcdd881e36af1e903264c
Andrew Smiley <andrew.smiley@live.com>
2014-06-02 13:04:44 -0400

    Initial Commit

---
+++ WirelessDeactivator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WirelessDeactivator
+//
+//  Created by Andrew Smiley on 6/2/14.
+//  Copyright (c) 2014 University of Cincinnati. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
