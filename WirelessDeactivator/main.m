4325f5dd5125228d96e7785c680c4a84aceda342
Andrew Smiley <andrew.smiley@live.com>
2014-06-02 13:14:14 -0400

    Inital upload

--- WirelessDeactivator/main.m
+++ WirelessDeactivator/main.m
@@ -7,8 +7,18 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <CoreWLAN/CoreWLAN.h>
 
 int main(int argc, const char * argv[])
 {
+    NSError *error;
+    NSArray *interfaces = [[CWInterface interfaceNames] allObjects];
+     for (int i = 0; i < [interfaces count]; i++) {
+         CWInterface *interface = [[CWInterface alloc] initWithInterfaceName:[interfaces objectAtIndex:i]];
+     [interface setPower:NO error:&error];
+     NSLog(@"%@", [interface interfaceName]);
+    
+}
+
     return NSApplicationMain(argc, argv);
 }
